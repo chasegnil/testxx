5683593630f4d1a04af6c29cad4ba5fe701c0d3b
chasegnil <gnil_hmz@126.com>
2018-04-15 17:25:08 +0800

    修改StringUtils类，还原到上一个版本

--- src/main/java/com/testxx/util/StringUtils.java
+++ src/main/java/com/testxx/util/StringUtils.java
@@ -2,11 +2,6 @@ package com.testxx.util;
 
 public class StringUtils {
 
-    /**
-     * 普通判断字符串是否为空的方法
-     * @param str
-     * @return
-     */
     public static boolean isEmpty(String str){
         return "".equals(str) || str == null;
     }
